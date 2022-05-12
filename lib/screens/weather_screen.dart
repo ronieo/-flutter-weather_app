f6b0e83922dea3f1606ac552f945ba13300f445b
ronieo <romelzen50@gmail.com>
2022-05-12 23:47:25 +0900

    weather ui macked

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:timer_builder/timer_builder.dart';
 
 class WeatherScreen extends StatefulWidget {
-  WeatherScreen({this.parseWeatherData});
+  const WeatherScreen({this.parseWeatherData});
   final dynamic parseWeatherData;
 
   @override
@@ -11,6 +13,8 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   late String cityName;
   late int temp;
+  late date = DateTime.now();
+
   @override
   void initState() {
     super.initState();
@@ -26,26 +30,222 @@ class _WeatherScreenState extends State<WeatherScreen> {
     print(cityName);
   }
 
+  DateFormat getSystemTime() {
+    var now = DateTime.now();
+    return DateFormat('h : mm a');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          //title: Text(''),
+          leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.near_me,
+                size: 30,
+              )),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.location_searching_outlined,
+                size: 30,
+              ),
+            ),
+          ]),
+     body: Container(
+        child: Stack(
           children: [
-            Text(
-              '$cityName',
-              style: TextStyle(fontSize: 30),
+            Image.asset(
+              'image/background.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
             ),
-            SizedBox(height: 20),
-            Text(
-              '$temp',
-              style: TextStyle(fontSize: 30),
-            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 150.0,
+                            ),
+                            Text(
+                              '$cityName',
+                               style:TextStyle(
+                                  fontSize: 35.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            Row(
+                              children: [
+                                TimerBuilder.periodic(
+                                  (Duration(minutes: 1)),
+                                  builder: (context) {
+                                    print('${getSystemTime()}');
+                                    return Text(
+                                      '${getSystemTime()}',
+                                       style:TextStyle(
+                                          fontSize: 16.0, color: Colors.white),
+                                    );
+                                  },
+                                ),
+                                Text(DateFormat(' - EEEE, ').format(date),
+                                     style:TextStyle(
+                                        fontSize: 16.0, color: Colors.white)),
+                                Text(DateFormat('d MMM, yyy').format(date),
+                                     style:TextStyle(
+                                        fontSize: 16.0, color: Colors.white))
+                              ],
+                            )
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '$temp\u2103',
+                               style:TextStyle(
+                                  fontSize: 85.0,
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.white),
+                            ),
+                            Row(
+                              children: [
+                                icon,
+                                SizedBox(
+                                  width: 10.0,
+                                ),
+                                Text(
+                                  '$des',
+                                  style:TextStyle(
+                                    fontSize: 16.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Divider(
+                        height: 15.0,
+                        thickness: 2.0,
+                        color: Colors.white30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'AQI(대기질지수)',
+                                 style:TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              airIcon,
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              airState,
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                '미세먼지',
+                                 style:TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '$dust1',
+                                 style:TextStyle(
+                                  fontSize: 24.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '㎍/m3',
+                                 style:TextStyle(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                '초미세먼지',
+                                 style:TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '$dust2',
+                                 style:TextStyle(
+                                  fontSize: 24.0,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                '㎍/m3',
+                                style:TextStyle(
+                                    fontSize: 14.0,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
-    ));
+    );
   }
 }
